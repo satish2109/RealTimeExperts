7c3e51e3147298ec334bdde17990ef4187f4311c
Lakshmipathi <realtimeExperts7batch@gmail.com>
2017-11-11 11:06:30 +0530

    Created  Initial project setup

---
+++ Sample/Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample
+//
+//  Created by Lakshmipathi on 11/11/17.
+//  Copyright © 2017 iPaveexperts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/Sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample
+//
+//  Created by Lakshmipathi on 11/11/17.
+//  Copyright © 2017 iPaveexperts. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
